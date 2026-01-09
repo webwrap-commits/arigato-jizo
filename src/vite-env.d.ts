f3fb1d175ce2abbcd88e112f54aadca63d8bf16c
webwrap-commits <webwrap@gmail.com>
2026-01-10 01:14:30 +0900

    Update vite-env.d.ts

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,7 @@
-/// <reference types="vite/client" />
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+export default defineConfig({
+  plugins: [react()],
+  base: './', // これを追加します
+})
